5ed4d542cf19654cff2424fb3182b4d4fb64c15c
uscen <farah.uscen@gmail.com>
2025-11-18 18:40:50 +0100

    feat(neovim): add rotate window command

--- win-dotfiles/app/local/nvim/after/ftplugin/help.lua
+++ win-dotfiles/app/local/nvim/after/ftplugin/help.lua
@@ -7,15 +7,10 @@ vim.opt_local.foldenable = false
 vim.opt_local.buflisted = false
 vim.opt_local.laststatus = 0
 -- Autocmds: =====================================================================================
-if vim.bo.filetype ~= 'help' then
-  return
-end
 vim.api.nvim_create_autocmd('BufWinEnter', {
   callback = function(event)
-    if vim.bo.filetype == 'help' then
-      vim.bo[event.buf].buflisted = false
-      vim.api.nvim_exec2('wincmd L', {})
-      vim.keymap.set('n', 'q', '<cmd>q<cr>', { buffer = event.buf, silent = true })
-    end
+    vim.bo[event.buf].buflisted = false
+    vim.api.nvim_exec2('wincmd L', {})
+    vim.cmd.wincmd('=')
   end,
 })
