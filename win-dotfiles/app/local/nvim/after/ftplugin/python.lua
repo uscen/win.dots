797a82f663aeaab62e8bd1492286e8263263715c
uscen <farah.uscen@gmail.com>
2025-11-25 15:00:00 -0800

    feat(neovim): add autocmds for convert js string automatic to template literals

--- win-dotfiles/app/local/nvim/after/ftplugin/python.lua
+++ win-dotfiles/app/local/nvim/after/ftplugin/python.lua
@@ -10,3 +10,31 @@ vim.g.pyindent_open_paren = 'shiftwidth()'
 vim.g.pyindent_continue = 'shiftwidth()'
 -- Mini: =========================================================================================
 vim.b.miniindentscope_config = { options = { border = 'top' } }
+-- Autocmds: =====================================================================================
+vim.api.nvim_create_augroup('py_fstring', { clear = true })
+vim.api.nvim_create_autocmd('InsertCharPre', {
+  group = 'py_fstring',
+  callback = function(opts)
+    -- Only run if f-string escape character is typed
+    if vim.v.char ~= '{' then return end
+
+    -- Get node and return early if not in a string
+    local node = vim.treesitter.get_node()
+
+    if not node then return end
+    if node:type() ~= 'string' then node = node:parent() end
+    if not node or node:type() ~= 'string' then return end
+
+    vim.print(node:type())
+    local row, col, _, _ = vim.treesitter.get_node_range(node)
+
+    -- Return early if string is already a format string
+    local first_char = vim.api.nvim_buf_get_text(opts.buf, row, col, row, col + 1, {})[1]
+    vim.print('row ' .. row .. ' col ' .. col)
+    vim.print("char: '" .. first_char .. "'")
+    if first_char == 'f' then return end
+
+    -- Otherwise, make the string a format string
+    vim.api.nvim_input("<Esc>m'" .. row + 1 .. 'gg' .. col + 1 .. "|if<Esc>`'la")
+  end,
+})
--- win-dotfiles/app/local/nvim/after/ftplugin/typescript.lua
+++ win-dotfiles/app/local/nvim/after/ftplugin/typescript.lua
@@ -1,4 +1,27 @@
 --              ╔═════════════════════════════════════════════════════════╗
 --              ║                       Typescript                        ║
 --              ╚═════════════════════════════════════════════════════════╝
--- Options: ======================================================================================
+-- Autocmds: =====================================================================================
+vim.api.nvim_create_augroup('js_template_string', { clear = true })
+vim.api.nvim_create_autocmd('InsertCharPre', {
+  group = 'js_template_string',
+  callback = function(opts)
+    -- Only run if template literal character is typed
+    if vim.v.char ~= '{' then return end
+
+    -- Get node and return early if not in a string
+    local node = vim.treesitter.get_node()
+
+    if not node then return end
+    if node:type() ~= 'string' then node = node:parent() end
+    if not node or node:type() ~= 'string' then return end
+
+    -- Return early if string is already a template literal
+    local row, col, _, _ = vim.treesitter.get_node_range(node)
+    local first_char = vim.api.nvim_buf_get_text(opts.buf, row, col, row, col + 1, {})[1]
+    if first_char == '`' then return end
+
+    -- Otherwise, change quotes to backticks using surround and return to insert mode
+    vim.api.nvim_input('<Esc>cs\"`' .. '<Esc>f{a')
+  end,
+})
