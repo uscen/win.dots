ac9110a5c1c2fe5afdac37c49d485d3870a75d6b
uscen <farah.uscen@gmail.com>
2025-12-09 02:49:01 -0800

    feat(neovim): add Better Ctrl-G

--- win-dotfiles/app/local/nvim/plugin/misc.lua
+++ win-dotfiles/app/local/nvim/plugin/misc.lua
@@ -333,3 +333,25 @@ end
 
 vim.api.nvim_create_user_command('RelativeFileNext', M.go_to_relative_file(1), {})
 vim.api.nvim_create_user_command('RelativeFilePrev', M.go_to_relative_file(-1), {})
+-- Better Ctrl-g: ==========================================================================
+function M.better_ctrl_g()
+  return function()
+    local pwd = vim.fn.getcwd()
+    local project_name
+    if vim.fn.isdirectory('.git') == 1 then
+      project_name = string.format('[%s]', vim.fn.fnamemodify(pwd, ':t'))
+    else
+      project_name = ''
+    end
+    local file = vim.fn.fnamemodify(vim.fn.expand('%'), ':.')
+    local messages = {}
+    if #project_name > 0 then
+      vim.list_extend(messages, { { project_name, 'Constant' }, { ' >> ', 'WarningMsg' } }, 1, 2)
+    end
+    vim.list_extend(
+      messages, { { pwd, 'Special' }, { ' >> ', 'WarningMsg' }, { file, 'Directory' } }, 1, 3)
+    vim.api.nvim_echo(messages, false, {})
+  end
+end
+
+vim.api.nvim_create_user_command('BetterCtrlG', M.better_ctrl_g(), {})
