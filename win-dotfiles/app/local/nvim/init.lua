30a0b7027339f20702f53ecb2e0905f9495d10d9
uscen <farah.uscen@gmail.com>
2025-12-10 04:15:48 -0800

    fix(neovim): all plugins orders

--- win-dotfiles/app/local/nvim/init.lua
+++ win-dotfiles/app/local/nvim/init.lua
@@ -129,6 +129,105 @@ later(function()
   vim.keymap.set('i', '<cr>', cr_action, { expr = true })
 end)
 --              ╭─────────────────────────────────────────────────────────╮
+--              │                       Mini.Completion                   │
+--              ╰─────────────────────────────────────────────────────────╯
+later(function()
+  -- enable Mini.Completion: =====================================================================
+  local MiniCompletion = require('mini.completion')
+  local process_items_opts = { kind_priority = { Text = -1, Snippet = 99 } }
+  local process_items = function(items, base)
+    return MiniCompletion.default_process_items(items, base, process_items_opts)
+  end
+  MiniCompletion.setup({
+    fallback_action = '<C-n>',
+    delay = { completion = 20, info = 20, signature = 10 },
+    window = { info = { border = 'single' }, signature = { border = 'single' } },
+    mappings = { force_twostep = '<C-n>', force_fallback = '<C-S-n>', scroll_down = '<C-f>', scroll_up = '<C-b>' },
+    lsp_completion = { source_func = 'omnifunc', auto_setup = false, process_items = process_items },
+  })
+  -- enable configured language servers 0.11: ====================================================
+  local capabilities = vim.lsp.protocol.make_client_capabilities()
+  capabilities = vim.tbl_deep_extend('force', capabilities, MiniCompletion.get_lsp_capabilities())
+  vim.lsp.config('*', { capabilities = capabilities })
+  vim.lsp.enable({ 'lua', 'html', 'css', 'emmet', 'json', 'typescript' })
+end)
+--              ╭─────────────────────────────────────────────────────────╮
+--              │                       Mini.Snippets                     │
+--              ╰─────────────────────────────────────────────────────────╯
+later(function()
+  local MiniSnippets    = require('mini.snippets')
+  -- Languge Patterns: ===========================================================================
+  local config_path     = vim.fn.stdpath('config')
+  local latex_patterns  = { 'latex/**/*.json', '**/latex.json' }
+  local markdown        = { 'markdown.json' }
+  local webHtmlPatterns = { 'html.json', 'ejs.json' }
+  local webJsTsPatterns = { 'web/javascript.json' }
+  local webPatterns     = { 'web/*.json' }
+  local lang_patterns   = {
+    tex = latex_patterns,
+    markdown_inline = markdown,
+    html = webHtmlPatterns,
+    ejs = webHtmlPatterns,
+    tsx = webPatterns,
+    javascriptreact = webPatterns,
+    typescriptreact = webPatterns,
+    javascript = webJsTsPatterns,
+    typescript = webJsTsPatterns,
+  }
+  -- Expand Patterns: ============================================================================
+  local match_strict    = function(snips)
+    -- Do not match with whitespace to cursor's left =============================================
+    -- return MiniSnippets.default_match(snips, { pattern_fuzzy = '%S+' })
+    -- Match exact from the start to the end of the string =======================================
+    return MiniSnippets.default_match(snips, { pattern_fuzzy = '^%S+$' })
+  end
+  -- Setup Snippets ==============================================================================
+  MiniSnippets.setup({
+    snippets = {
+      MiniSnippets.gen_loader.from_file(config_path .. '/snippets/global.json'),
+      MiniSnippets.gen_loader.from_lang({ lang_patterns = lang_patterns }),
+    },
+    mappings = { expand = '<C-e>', jump_next = '<C-l>', jump_prev = '<C-h>', stop = '<C-c>' },
+    expand   = {
+      match = match_strict,
+      insert = function(snippet)
+        return MiniSnippets.default_insert(snippet, { empty_tabstop = '', empty_tabstop_final = '†' })
+      end,
+    },
+  })
+  -- Expand Snippets Or complete by Tab ==========================================================
+  local expand_or_complete = function()
+    if #MiniSnippets.expand({ insert = false }) > 0 then
+      vim.schedule(MiniSnippets.expand); return ''
+    end
+    return vim.fn.pumvisible() == 1 and
+        (vim.fn.complete_info().selected == -1 and vim.keycode('<c-n><c-y>') or vim.keycode('<c-y>')) or '<Tab>'
+  end
+  vim.keymap.set('i', '<Tab>', expand_or_complete, { expr = true, replace_keycodes = true })
+  -- exit snippet sessions on entering normal mode: ==============================================
+  vim.api.nvim_create_autocmd('User', {
+    pattern = 'MiniSnippetsSessionStart',
+    callback = function()
+      vim.api.nvim_create_autocmd('ModeChanged', {
+        pattern = '*:n',
+        once = true,
+        callback = function()
+          while MiniSnippets.session.get() do
+            MiniSnippets.session.stop()
+          end
+        end,
+      })
+    end,
+  })
+  -- exit snippets upon reaching final tabstop: ==================================================
+  vim.api.nvim_create_autocmd('User', {
+    pattern = 'MiniSnippetsSessionJump',
+    callback = function(args)
+      if args.data.tabstop_to == '0' then MiniSnippets.session.stop() end
+    end,
+  })
+end)
+--              ╭─────────────────────────────────────────────────────────╮
 --              │                         Mini.Ai                         │
 --              ╰─────────────────────────────────────────────────────────╯
 later(function()
@@ -376,105 +475,6 @@ later(function()
     local buffer_mappings = { wipeout = { char = '<C-d>', func = wipeout_cur } }
     MiniPick.builtin.buffers(local_opts, { mappings = buffer_mappings })
   end
-end)
---              ╭─────────────────────────────────────────────────────────╮
---              │                       Mini.Completion                   │
---              ╰─────────────────────────────────────────────────────────╯
-later(function()
-  -- enable Mini.Completion: =====================================================================
-  local MiniCompletion = require('mini.completion')
-  local process_items_opts = { kind_priority = { Text = -1, Snippet = 99 } }
-  local process_items = function(items, base)
-    return MiniCompletion.default_process_items(items, base, process_items_opts)
-  end
-  MiniCompletion.setup({
-    fallback_action = '<C-n>',
-    delay = { completion = 20, info = 20, signature = 10 },
-    window = { info = { border = 'single' }, signature = { border = 'single' } },
-    mappings = { force_twostep = '<C-n>', force_fallback = '<C-S-n>', scroll_down = '<C-f>', scroll_up = '<C-b>' },
-    lsp_completion = { source_func = 'omnifunc', auto_setup = false, process_items = process_items },
-  })
-  -- enable configured language servers 0.11: ====================================================
-  local capabilities = vim.lsp.protocol.make_client_capabilities()
-  capabilities = vim.tbl_deep_extend('force', capabilities, MiniCompletion.get_lsp_capabilities())
-  vim.lsp.config('*', { capabilities = capabilities })
-  vim.lsp.enable({ 'lua', 'html', 'css', 'emmet', 'json', 'typescript' })
-end)
---              ╭─────────────────────────────────────────────────────────╮
---              │                       Mini.Snippets                     │
---              ╰─────────────────────────────────────────────────────────╯
-later(function()
-  local MiniSnippets    = require('mini.snippets')
-  -- Languge Patterns: ===========================================================================
-  local config_path     = vim.fn.stdpath('config')
-  local latex_patterns  = { 'latex/**/*.json', '**/latex.json' }
-  local markdown        = { 'markdown.json' }
-  local webHtmlPatterns = { 'html.json', 'ejs.json' }
-  local webJsTsPatterns = { 'web/javascript.json' }
-  local webPatterns     = { 'web/*.json' }
-  local lang_patterns   = {
-    tex = latex_patterns,
-    markdown_inline = markdown,
-    html = webHtmlPatterns,
-    ejs = webHtmlPatterns,
-    tsx = webPatterns,
-    javascriptreact = webPatterns,
-    typescriptreact = webPatterns,
-    javascript = webJsTsPatterns,
-    typescript = webJsTsPatterns,
-  }
-  -- Expand Patterns: ============================================================================
-  local match_strict    = function(snips)
-    -- Do not match with whitespace to cursor's left =============================================
-    -- return MiniSnippets.default_match(snips, { pattern_fuzzy = '%S+' })
-    -- Match exact from the start to the end of the string =======================================
-    return MiniSnippets.default_match(snips, { pattern_fuzzy = '^%S+$' })
-  end
-  -- Setup Snippets ==============================================================================
-  MiniSnippets.setup({
-    snippets = {
-      MiniSnippets.gen_loader.from_file(config_path .. '/snippets/global.json'),
-      MiniSnippets.gen_loader.from_lang({ lang_patterns = lang_patterns }),
-    },
-    mappings = { expand = '<C-e>', jump_next = '<C-l>', jump_prev = '<C-h>', stop = '<C-c>' },
-    expand   = {
-      match = match_strict,
-      insert = function(snippet)
-        return MiniSnippets.default_insert(snippet, { empty_tabstop = '', empty_tabstop_final = '†' })
-      end,
-    },
-  })
-  -- Expand Snippets Or complete by Tab ==========================================================
-  local expand_or_complete = function()
-    if #MiniSnippets.expand({ insert = false }) > 0 then
-      vim.schedule(MiniSnippets.expand); return ''
-    end
-    return vim.fn.pumvisible() == 1 and
-        (vim.fn.complete_info().selected == -1 and vim.keycode('<c-n><c-y>') or vim.keycode('<c-y>')) or '<Tab>'
-  end
-  vim.keymap.set('i', '<Tab>', expand_or_complete, { expr = true, replace_keycodes = true })
-  -- exit snippet sessions on entering normal mode: ==============================================
-  vim.api.nvim_create_autocmd('User', {
-    pattern = 'MiniSnippetsSessionStart',
-    callback = function()
-      vim.api.nvim_create_autocmd('ModeChanged', {
-        pattern = '*:n',
-        once = true,
-        callback = function()
-          while MiniSnippets.session.get() do
-            MiniSnippets.session.stop()
-          end
-        end,
-      })
-    end,
-  })
-  -- exit snippets upon reaching final tabstop: ==================================================
-  vim.api.nvim_create_autocmd('User', {
-    pattern = 'MiniSnippetsSessionJump',
-    callback = function(args)
-      if args.data.tabstop_to == '0' then MiniSnippets.session.stop() end
-    end,
-  })
 end)
 --              ╭─────────────────────────────────────────────────────────╮
 --              │                        Mini.Files                       │
