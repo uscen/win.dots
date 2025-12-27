c07a49ccb86bf018e2b2dc14dcc52666a9a3cc24
uscen <farah.uscen@gmail.com>
2025-12-27 14:59:06 -0800

    fix(neovim): use same height window for all

--- win-dotfiles/app/local/nvim/init.lua
+++ win-dotfiles/app/local/nvim/init.lua
@@ -1619,7 +1619,7 @@ later(function()
   end, { nargs = '?', complete = 'dir' })
   -- Open a scratch buffer: ======================================================================
   vim.api.nvim_create_user_command('Scratch', function()
-    vim.cmd 'bel 8new'
+    vim.cmd 'bel 10new'
     local buf = vim.api.nvim_get_current_buf()
     for name, value in pairs { filetype = 'scratch', buftype = 'nofile', bufhidden = 'wipe', swapfile = false, modifiable = true } do
       vim.api.nvim_set_option_value(name, value, { buf = buf })
